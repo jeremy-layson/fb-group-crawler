baf96663107d3f8fbfa31f040a5e1db80f98a941
Jeremy Layson <jeremy.b.layson@gmail.com>
2018-05-20 12:05:34 +0800

    Added new columns to group members

---
+++ database/migrations/2018_05_20_033200_add_columns_to_group_members.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsToGroupMembers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('group_members', function (Blueprint $table) {
+            $table->string('activity')->after('member_name')->default('unprocessed');
+            $table->boolean('kicked')->after('activity')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('group_members', function (Blueprint $table) {
+            $table->dropColumn(['activity', 'kicked']);
+        });
+    }
+}
